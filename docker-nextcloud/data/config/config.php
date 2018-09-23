3a949183f55fc10dd1105f9b3cdae24466fe2e02
gmoulard <guillaume@moulard.org>
2018-09-23 17:10:47 +0200

    Create config.php

---
+++ docker-nextcloud/data/config/config.php
@@ -0,0 +1,36 @@
+<?php
+$CONFIG = array (
+  'passwordsalt' => 'rtCtOxa/vxAQFbGZo5hZ35HGXr8fwA',
+  'secret' => '4rUBfPUoIYSBWZWEXboHbGacmdBoY8sttkGkj24UW7nyPgMf',
+  'trusted_domains' =>
+  array (
+    0 => 'localhost',
+    2 => '172.17.0.3',
+    3 => 'pi3b.moulard.org',
+    4 => '192.168.1.49',
+    5 => 'pi3b.pi-appliance.com',
+    6 => '10.100.0.3',
+  ),
+  'datadirectory' => '/data/app',
+  'overwrite.cli.url' => 'http://pi3b.moulard.org',
+  'dbtype' => 'mysql',
+  'version' => '12.0.0.29',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'ncadmin',
+  'dbpassword' => 'ownyourbits',
+  'installed' => true,
+  'instanceid' => 'oczb2i9a656w',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'trusted_proxies' =>
+  array (
+    0 => '10.100.0.3',
+  ),
+  'overwritehost' => 'pi3b.moulard.org',
+  'overwriteprotocol' => 'https',
+  'overwritewebroot' => '/nextcloud',
+  'overwritecondaddr' => '^192.168.0.100$',
+  ‘overwrite.cli.url’ => ‘https://pi3b.moulard.org/nextcloud/’,
+);
