00756c1596b9c28c4af557b2f32cdfcdfa708056
gmoulard <guillaume@moulard.org>
2018-10-11 20:08:46 +0200

    Create monfichier.c

---
+++ verifCode/monfichier.c
@@ -0,0 +1,17 @@
+
+
+2.1 OK
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+2.1 KO
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaax
+
+2.2 OK
+    
+2.2 KO
+	
+2.3 KO
+* {
+* main
+* if
+// if
+
