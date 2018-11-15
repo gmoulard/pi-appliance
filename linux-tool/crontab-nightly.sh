756613ad388c4ebfa156fcc532de8a5cff8771cb
gmoulard <gmoulard@gmail.com>
2018-11-15 13:38:06 +0100

    Update crontab-nightly.sh

--- linux-tool/crontab-nightly.sh
+++ linux-tool/crontab-nightly.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+
+sudo ~/pi-appliance/linux-tool/clean_log.sh
+
 date
 
 ~/pi-appliance/linux-tool/bkp.sh
 
 ~/pi-appliance/linux-tool/dynHost/dynhost.sh
 ~/pi-appliance/linux-tool/clean_log.sh
-rsync -vrhz --progress pi3:/var/www/html/DD/Photo/ /var/www/html/DD/Photo/
+
+#rsync -avz pi3://media/sda/Photo  /media/sda
+#rsync -avz /media/sda/Photo pi3://media/sda
+
 
 sudo /sbin/reboot
