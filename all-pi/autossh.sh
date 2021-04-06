1fb9f1653f77a10c5543ef8958165b1e81bc31ee
gmoulard <guillaume@moulard.org>
2021-04-07 00:34:13 +0200

    Update autossh.sh

--- all-pi/autossh.sh
+++ all-pi/autossh.sh
@@ -8,7 +8,7 @@
 # https://www.raspberrypi.org/documentation/hardware/raspberrypi/revision-codes/README.md
 # $ sudo crontab -e
 # @reboot /home/pi/pi-appliance/autossh.sh >> /var/log/pi-appliance.log 2>> /var/log/pi-appliance.err
-
+# curl https://raw.githubusercontent.com/gmoulard/pi-appliance/master/all-pi/autossh.sh > /home/pi/pi-appliance/autossh.sh
 
 case $HOSTNAME in
   "vgm")   export AUTO_PORT=" -R 1301:*:22 " ;;
