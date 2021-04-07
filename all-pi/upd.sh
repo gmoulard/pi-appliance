1e070c957835cb0011e6dcb9bab04ca5810e7a3a
gmoulard <guillaume@moulard.org>
2021-04-07 23:10:53 +0200

    Update upd.sh

--- all-pi/upd.sh
+++ all-pi/upd.sh
@@ -10,12 +10,15 @@ cat /sys/firmware/devicetree/base/model
 temp_file=$(mktemp)
 REPO="https://raw.githubusercontent.com/gmoulard/pi-appliance/master/all-pi"
 
-if [ `cat /sys/firmware/devicetree/base/model | grep Raspberry | wc -l` != 1 ]; then
+if [ `cat /sys/firmware/devicetree/base/model | grep Raspberry | wc -l` -ne 1 ]; then
     echo on est pas sur un pi, Pbs !!!!
     exit 0
 fi
 
-mkdir ~/pi-appliance
+if [ ! -d ~/pi-appliance ]; then 
+    mkdir ~/pi-appliance
+fi
+
 curl -s $REPO/upd.sh > ~/pi-appliance/upd.sh
 curl -s $REPO/autossh.sh > ~/pi-appliance/autossh.sh
 curl -s $REPO/bkp.sh > ~/pi-appliance/bkp.sh
