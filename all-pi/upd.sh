a49d14198e249df9204804575e1461653a88c103
gmoulard <guillaume@moulard.org>
2021-04-07 23:03:53 +0200

    Update upd.sh

--- all-pi/upd.sh
+++ all-pi/upd.sh
@@ -7,9 +7,28 @@
 echo Begin: `date` 
 cat /sys/firmware/devicetree/base/model
 
+temp_file=$(mktemp)
+
+if [ `cat /sys/firmware/devicetree/base/model | grep Raspberry | wc -l` != 1 ]; then
+    echo on est pas sur un pi, Pbs !!!!
+    exit 0
+fi
+
 export REPO="https://raw.githubusercontent.com/gmoulard/pi-appliance/master/all-pi"
+mkdir ~/pi-appliance
+curl -s $REPO/autossh.sh > ~/pi-appliance/autossh.sh
+curl -s $REPO/bkp.sh > ~/pi-appliance/bkp.sh
+curl -s $REPO/config > ~/.ssh/config
 
-curl -s $REPO/autossh.sh > /home/pi/pi-appliance/autossh.sh
-curl -s $REPO/bkp.sh > /home/pi/pi-appliance/bkp.sh
+curl -s $REPO/authorized_keys >> ~/.ssh/authorized_keys
+sort -u ~/.ssh/authorized_keys > ${temp_file}
+cat ${temp_file} > ~/.ssh/authorized_keys
 
+# crontab pi
+# 1 1 * * * /home/pi/upd.sh 
+#crontab root
+#4 4 * * *  apt-get update ; apt-get -y upgrade ; reboot
+#@reboot /home/pi/pi-appliance/autossh.sh >> /var/log/pi-appliance.log 2>> /var/log/pi-appliance.err
+
+rm ${temp_file}
 echo End: `date` 
